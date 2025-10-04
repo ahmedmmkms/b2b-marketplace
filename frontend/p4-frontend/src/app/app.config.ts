05e5ece4e02ad6676dcd451b0b187a18018b13be
Ahmed Mahmoud <ahmedmmkms@hotmail.com>
2025-10-05 00:44:29 +0300

    Fix NG0203 error by adding ng-zorro-antd dependencies and configuration

--- frontend/p4-frontend/src/app/app.config.ts
+++ frontend/p4-frontend/src/app/app.config.ts
@@ -6,6 +6,8 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 
 import { routes } from './app.routes';
 
@@ -18,6 +20,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
+    provideNzIcons(),
+    { provide: NZ_I18N, useValue: en_US },
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
